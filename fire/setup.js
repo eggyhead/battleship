4ba1999ec13e26d5da9ef2f5959dcbdb4909bc83
Erika Weil <erikaweil@Erikas-MacBook-Pro.local>
2018-03-29 16:08:50 -0500

    first commit

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyACZ3ozcfcN4gtUGTq9UuolGMVHmlAjWZs",
+  "databaseURL": "https://battleship-3a46c.firebaseio.com",
+  "storageBucket": "battleship-3a46c.appspot.com",
+  "authDomain": "battleship-3a46c.firebaseapp.com",
+  "messagingSenderId": "664271444561",
+  "projectId": "battleship-3a46c"
+});
+
